bec1878e5a39e2f4bf3446c1f63faf69c06a35aa
Milad Shalikarian <m.shalikarian@gmail.com>
2024-08-20 20:02:12 +0100

    spotify class & searchBar design

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { IoSearchSharp } from 'react-icons/io5';
 import styles from './searchBar.module.css';
 const SearchBar = () => {
-  return <div>this is search bar</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <h2>
+            jam<span className={styles.span}>m</span>ming
+          </h2>
+        </div>
+        <div className={styles.inputContainer}>
+          <input className={styles.input} type='text' placeholder='Search' />
+          <div className={styles.icon}>
+            <IoSearchSharp />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SearchBar;
