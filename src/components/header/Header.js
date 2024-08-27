4e9121c9a9d60d1503cea921aaf5e7a5f0672e4d
Milad Shalikarian <m.shalikarian@gmail.com>
2024-08-27 15:22:00 +0100

    creating header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,6 +8,10 @@ const Header = () => {
     <div>
       <div className={styles.username}>
         <h2>Welcome {userName}</h2>
+        <p className={styles.p}>
+          Browse Spotify, search by music title, the artist or both, and create
+          your own playlist
+        </p>
       </div>
     </div>
   );
